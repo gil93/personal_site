5271f213d980b8076a09dabe045ff3565dd43402
Gil <gvelezjr02@gmail.com>
2017-03-27 04:07:08 -0400

    whitespace

--- js/modules/containers/index.js
+++ js/modules/containers/index.js
@@ -116,14 +116,23 @@ export default class Index extends React.Component {
 						<div class="wave-container">
 
 							<svg viewBox="0 0 100 20">
-							  <defs>
-							    <pattern id="wave" x="0" y="0" width="100" height="20" patternUnits="userSpaceOnUse">
-							      <path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" fill="#000000"> 
-							        <animateTransform attributeName="transform" type="translate" begin="0s" dur="1.5s" from="0,0" to="40,0" repeatCount="indefinite" />
-							      </path>
-							    </pattern>
-							  </defs>
-							  <polygon points="0,0 100,0 100,20 0,20" fill="url(#wave)"></polygon>
+
+								<defs>
+
+							    	<pattern id="wave" x="0" y="0" width="100" height="20" patternUnits="userSpaceOnUse">
+
+							    		<path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" fill="#000000">
+
+							        		<animateTransform attributeName="transform" type="translate" begin="0s" dur="1.5s" from="0,0" to="40,0" repeatCount="indefinite" />
+
+							    		</path>
+
+							    	</pattern>
+
+								</defs>
+
+								<polygon points="0,0 100,0 100,20 0,20" fill="url(#wave)"></polygon>
+
 							</svg>
 
 						</div>
