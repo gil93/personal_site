8bd57712e0d49bed06c68f1bee6172fecc42defa
Gil <gvelezjr02@gmail.com>
2017-03-27 04:01:35 -0400

    add scroll reducer to combined reducers

--- js/modules/store.js
+++ js/modules/store.js
@@ -3,10 +3,12 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import admin_reducer from './reducers/admin';
+import scroll_reducer from './reducers/scroll';
 
 const reducers = combineReducers({
 
-	admin: admin_reducer
+	'admin': admin_reducer,
+	'scroll': scroll_reducer
 
 });
 
